0cf922bd1e87bd012a5004043e8aa73ef752c98b
muthuU234 <muthuselvarajan24@gmail.com>
2025-07-09 13:31:53 +0530

    Update sample.java
    newlineadded

--- sample.java
+++ sample.java
@@ -1,8 +1,8 @@
 class new1{
     public static void main(String[] args) {
         System.out.println("hello java");
-        System.ouot.println("programming language...");
-                System.ouot.println(" new programming language...");
+        System.out.println("programming language...");
+                System.out.println(" new programming language...");
 
     }
 }
