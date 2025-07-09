c1b27a49b8be1ae160562c12dac8a22b44575ed5
muthuU234 <muthuselvarajan24@gmail.com>
2025-07-09 13:30:15 +0530

    another line added

--- sample.java
+++ sample.java
@@ -2,5 +2,7 @@ class new1{
     public static void main(String[] args) {
         System.out.println("hello java");
         System.ouot.println("programming language...");
+                System.ouot.println(" new programming language...");
+
     }
 }
